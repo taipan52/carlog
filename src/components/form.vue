f214b7728afb73b49d81d2a182893b8397d6b72d
Суханов Антон Александрович <SuhanovAA@zoo.ru>
2021-10-19 16:36:52 +0300

    started

---
+++ src/components/form.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        Форма тут<br>
+        <span @click="test">Тест</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Form',
+    props: {
+        msg: String
+    },
+    methods: {
+
+        test() {
+
+            console.log(this.$parent);
+
+        },
+
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+
+import Vuex from 'vuex'
+import 'es6-promise/auto';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import vSelect from "vue-select";
+import DatePicker from 'vue2-datepicker';
+
+//сам объект
 import App from './App.vue'
+import store from './store'
+
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex)
+Vue.use(VueAxios, axios)
+Vue.component('v-select', vSelect);
+Vue.component('date-picker', DatePicker);
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
